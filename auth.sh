8d8b372b1044101b6e614ccb6ca4249e29339a47
Hao Song <songhao@cmee.zjxu.edu.cn>
2016-04-04 13:52:53 +0800

    Update auth.sh

--- auth.sh
+++ auth.sh
@@ -1,1 +1,2 @@
-~/authReConnect/auth 10.100.1.251 w20061109 804004
+#~/authReConnect/auth 10.100.1.251 w20061109 804004
+curl -X POST --data "action=login&username=w20061109&password=804004&ac_id=1&user_ip=&nas_ip=&user_mac=&persistent_remember_me=1&save_me=1&ajax=1" "http://10.100.1.251:803/include/auth_action.php"
